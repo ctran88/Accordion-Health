0077d6d2f773510c2430fd58d796ff9ec62538f5
Chris Tran <christopher.v.tran@gmail.com>
2015-12-26 16:51:10 -0600

    Updated file
    Updated reference files; deleted unnecessary files.  Parse file updated
    to reflect new structure.  Taxonomy Categories file reflects newly
    categorized taxonomy codes for medical doctors.
    
    **Taxonomy Index file is not updated to match Taxonomy Categories

--- parse_doctors.py
+++ parse_doctors.py
@@ -1,54 +1,79 @@
 from multiprocessing import Pool
+import sys, getopt
 
 # These globals represent corresponding index locations inside the CSV we parse.
+# TODO: Add the field indexes we need
+NPI_FIELD = 0
 LAST_NAME_FIELD = 5
 FIRST_NAME_FIELD = 6
 MIDDLE_NAME_FIELD = 7
-NPI_FIELD = 0
-ORGANIZATION_FIELD = 4
 TAXONOMY_FIELD = 47
+LICENSE_FIELD = 48
+
+taxonomy_dict = {}
+
 
 class USAddress:
     """ A simple address data structure """
     street_1 = ""
     street_2 = ""
-    city     = ""
-    state    = ""
+    city = ""
+    state = ""
     zip_code = ""
+
 
 class MedicalDoctor:
     """ A simple doctor data structure """
-	last_name      = ""
-	first_name     = ""
-	middle_name    = ""
-	organization   = ""
-    address        = USAddress()
-	npi            = ""
-    taxonomy       = ""
+    first_name = ""
+    last_name = ""
+    middle_name = ""
+    address = USAddress()
+    taxonomy = ""
     specialization = ""
-    tags           = ""
+    npi = ""
+    license = ""
+    tags = ""
 
-    def __init__(self, taxonomy, specialization, tags):
-        self.taxonomy       = taxonomy
+    def __init__(self, taxonomy, specialization):
+        self.taxonomy = taxonomy
         self.specialization = specialization
-        self.tags           = tags
 
-# Possible taxonomy index structure
-taxonomies = {'207X00000X': MedicalDoctor('207X00000X', 'Orthopaedic Surgery', ['trauma', 'sports injuries', 'degenerative disease', 'infection', 'tumor', 'congenital disorder'])}
 
-def parse_line(line):
-    items = line.split(',')
+def build_taxonomy_dict(taxonomy_csv):
+    fp = open(taxonomy_csv)
+    for i, line in enumerate(fp):
+        if not i == 0:
+            items = line.split(',')
+            taxonomy_dict[items[0]] = items[1]
+    fp.close()
 
-    try:
-        doctor = taxonomies[items[TAXONOMY_FIELD]]
-        doctor.name_and_title = "test"
-        return doctor
 
-    except:
-        # Taxonomy not found in list
-        return None
+def parse_doctors(doctors_csv):
+    fp = open(doctors_csv)
+    for i, line in enumerate(fp):
+        try:
+            items = line.split(',')
+            taxonomy = items[TAXONOMY_FIELD]
+            taxonomy_description = taxonomy_dict[taxonomy]
 
-fp = open("/Users/THANATOS/Downloads/test.csv")
-for i, line in enumerate(fp):
-    parse_line(line)
-fp.close()
+            doctor = MedicalDoctor(taxonomy, taxonomy_description)
+
+            #TODO: Fill in the address and name information of the doctor struct
+
+            # insert into DB
+        except:
+            # Taxonomy not found in list
+            pass
+    fp.close()
+
+
+def main(argv):
+    if not len(argv) == 2:
+        print("Useage: parse_doctors.py <doctors.csv> <taxonomy_codes.csv>")
+    else:
+        build_taxonomy_dict(argv[1])
+        parse_doctors(argv[0])
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- taxonomy.py
+++
@@ -1,6 +0,0 @@
-import csv
-f = open('Taxonomy.csv')
-csv_f = csv.DictReader(f)
-
-for row in csv_f:
-	print row

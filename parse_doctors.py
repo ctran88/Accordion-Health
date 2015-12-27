859e6da49f0b99add3702202aa086a6db1bd12c5
Chris Tran <christopher.v.tran@gmail.com>
2015-12-26 20:40:18 -0600

    Updated parse script
    Made lots of updates to parse script to fill in doctor information and
    include global variables for references

--- parse_doctors.py
+++ parse_doctors.py
@@ -1,14 +1,18 @@
 from multiprocessing import Pool
 import sys, getopt
 
-# These globals represent corresponding index locations inside the CSV we parse.
-# TODO: Add the field indexes we need
+# These globals represent corresponding index locations inside the CSV.
 NPI_FIELD = 0
 LAST_NAME_FIELD = 5
 FIRST_NAME_FIELD = 6
 MIDDLE_NAME_FIELD = 7
 TAXONOMY_FIELD = 47
 LICENSE_FIELD = 48
+STREET_1_FIELD = 28
+STREET_2_FIELD = 29
+CITY_FIELD = 30
+STATE_FIELD = 31
+ZIP_CODE_FIELD = 32
 
 taxonomy_dict = {}
 
@@ -37,6 +41,17 @@ class MedicalDoctor:
     def __init__(self, taxonomy, specialization):
         self.taxonomy = taxonomy
         self.specialization = specialization
+    
+    def __repr__(self):
+        # Problem with newline after Specialization
+        return """ Doctor Information:
+                   First Name: %s
+                   Middle Name: %s
+                   Last Name: %s
+                   Taxonomy: %s
+                   Specialization: %s
+                   NPI: %s
+                   License: %s """ % (self.first_name, self.middle_name, self.last_name, self.taxonomy, self.specialization, self.npi, self.license)
 
 
 def build_taxonomy_dict(taxonomy_csv):
@@ -55,10 +70,27 @@ def parse_doctors(doctors_csv):
             items = line.split(',')
             taxonomy = items[TAXONOMY_FIELD]
             taxonomy_description = taxonomy_dict[taxonomy]
-
+			
+            # Fills out address class instance
+            address = USAddress()
+            address.street_1 = items[STREET_1_FIELD]
+            address.street_2 = items[STREET_2_FIELD]
+            address.city = items[CITY_FIELD]
+            address.state = items[STATE_FIELD]
+            address.zip_code = items[ZIP_CODE_FIELD]
+			
+            # Fills out doctor class instance
             doctor = MedicalDoctor(taxonomy, taxonomy_description)
-
-            #TODO: Fill in the address and name information of the doctor struct
+            doctor.first_name = items[FIRST_NAME_FIELD]
+            doctor.last_name = items[LAST_NAME_FIELD]
+            doctor.middle_name = items[MIDDLE_NAME_FIELD]
+            doctor.npi = items[NPI_FIELD]
+            doctor.license = items[LICENSE_FIELD]
+			
+			# For validation of doctor information
+            print doctor
+			
+            # TODO: Fill in the address and name information of the doctor struct
 
             # insert into DB
         except:
@@ -69,7 +101,8 @@ def parse_doctors(doctors_csv):
 
 def main(argv):
     if not len(argv) == 2:
-        print("Useage: parse_doctors.py <doctors.csv> <taxonomy_codes.csv>")
+		# Useage text reflects test data
+        print("Useage: parse_doctors.py <doctors_test.csv> <taxonomy_test.csv>")
     else:
         build_taxonomy_dict(argv[1])
         parse_doctors(argv[0])

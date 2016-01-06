c2da5df2d1ff97a6fd43b6f4aa53adeaed097088
ctran88 <christopher.v.tran@gmail.com>
2016-01-06 14:33:48 -0600

    Update to parse
    Working on attaching coordinates to doctor dictionary to be imported
    into mongo

--- Back-end/NPPES_parse.py
+++ Back-end/NPPES_parse.py
@@ -4,7 +4,9 @@ import csv
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 
-# These globals represent corresponding index locations inside the CSV.
+TAX_CODE = 0
+TAX_DESC = 1
+
 NPI_FIELD = 0
 LAST_NAME_FIELD = 5
 FIRST_NAME_FIELD = 6
@@ -17,11 +19,24 @@ CITY_FIELD = 30
 STATE_FIELD = 31
 ZIP_CODE_FIELD = 32
 
+client = MongoClient()
+db = client.testdb2
+coll = db.testcollection
+
+coord_dict = {}
 taxonomy_dict = {}
 
-client = MongoClient()
-db = client.testdb
-coll = db.testcollection
+
+def build_coord_dict(coord_csv):
+    with open(coord_csv) as fp:
+        reader = csv.reader(fp)
+        next(reader, None)
+        for row in reader:
+            state = row[0]
+            coord_dict.setdefault(state, []).append(row[1:])
+        """for keys, values in coord_dict.items():
+            print(keys)
+            print(values)"""
 
 
 def build_taxonomy_dict(taxonomy_csv):
@@ -29,7 +44,7 @@ def build_taxonomy_dict(taxonomy_csv):
         reader = csv.reader(fp)
         next(reader, None)
         for row in reader:
-            taxonomy_dict[row[0]] = row[1]
+            taxonomy_dict[row[TAX_CODE]] = row[TAX_DESC]
 
 
 def build_doctor_dict(doctors_csv):
@@ -41,7 +56,6 @@ def build_doctor_dict(doctors_csv):
                 taxonomy = row[TAXONOMY_FIELD]
                 taxonomy_description = taxonomy_dict[taxonomy]
 
-                # TODO: Still need to import address information
                 doctor_dict = {'NPI': row[NPI_FIELD],
                                'Last Name': row[LAST_NAME_FIELD],
                                'First Name': row[FIRST_NAME_FIELD],
@@ -53,26 +67,32 @@ def build_doctor_dict(doctors_csv):
                                            'Street_2': row[STREET_2_FIELD],
                                            'City': row[CITY_FIELD],
                                            'State': row[STATE_FIELD],
-                                           'Zip Code': row[ZIP_CODE_FIELD]}}
-                coll.insert(doctor_dict)
+                                           'Zip Code': row[ZIP_CODE_FIELD]}
+                               }
+
+                coord_state = doctor_dict.get('Address', None).get('State', None)
+                coord_city = doctor_dict.get('Address', None).get('City', None)
+                lat = coord_dict[coord_state][coord_city]
+                # lng = coord_dict[coord_state][coord_city]
+                # coll.insert(doctor_dict)
             except:
                 # TODO: exception handling needed
                 pass
 
 
 def main(argv):
-    if not len(argv) == 2:
-        # Usage text reflects test data
-        print("Usage: parse_doctors.py <doctors_test.csv> <taxonomy_test.csv>")
+    if not len(argv) == 3:
+        print("Usage: NPPES_parse.py <doctors_csv.csv> <taxonomy_csv.csv> <coordinates_csv.csv>")
     else:
         try:
             # TODO: is this part needed?
-            #client = MongoClient()
+            # client = MongoClient()
             print ("Connected to MongoDB successfully.")
         except ConnectionFailure, e:
             sys.stderr.write("Could not connect to MongoDB: %s" % e)
             sys.exit(1)
 
+        build_coord_dict(argv[2])
         build_taxonomy_dict(argv[1])
         build_doctor_dict(argv[0])
 

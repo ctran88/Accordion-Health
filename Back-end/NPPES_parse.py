479b6b234f0bba0d4785ceedb381a6be5f269986
ctran88 <christopher.v.tran@gmail.com>
2016-01-07 05:07:05 -0600

    Updated parse
    Finished coordinate dictionary and attaching the lat/lng to the doctors
    in the database.
    
    Still having trouble with matching city names due to formatting

--- Back-end/NPPES_parse.py
+++ Back-end/NPPES_parse.py
@@ -2,11 +2,13 @@
 import sys
 import csv
 from pymongo import MongoClient
-from pymongo.errors import ConnectionFailure
+# from pymongo.errors import ConnectionFailure
 
+# Constants for taxonomy function
 TAX_CODE = 0
 TAX_DESC = 1
 
+# Constants for doctor function
 NPI_FIELD = 0
 LAST_NAME_FIELD = 5
 FIRST_NAME_FIELD = 6
@@ -19,26 +21,28 @@ CITY_FIELD = 30
 STATE_FIELD = 31
 ZIP_CODE_FIELD = 32
 
+# Global MongoDB variables
 client = MongoClient()
-db = client.testdb2
-coll = db.testcollection
+db = client.testdb
+coll = db.testcollection2
 
+# Global coordinate and taxonomy dictionaries
 coord_dict = {}
 taxonomy_dict = {}
 
 
+# This function builds a dictionary to reference all US city coordinates
 def build_coord_dict(coord_csv):
     with open(coord_csv) as fp:
         reader = csv.reader(fp)
         next(reader, None)
         for row in reader:
-            state = row[0]
-            coord_dict.setdefault(state, []).append(row[1:])
-        """for keys, values in coord_dict.items():
-            print(keys)
-            print(values)"""
+            coord_dict[row[0]] = coord_dict.get(row[0], {})
+            coord_dict[row[0]][row[1]] = coord_dict[row[0]].get(row[1], [])
+            coord_dict[row[0]][row[1]].append(row[2:])
 
 
+# This function builds a dictionary to reference all January 2016 taxonomy codes
 def build_taxonomy_dict(taxonomy_csv):
     with open(taxonomy_csv) as fp:
         reader = csv.reader(fp)
@@ -47,6 +51,7 @@ def build_taxonomy_dict(taxonomy_csv):
             taxonomy_dict[row[TAX_CODE]] = row[TAX_DESC]
 
 
+# This function builds one dictionary per doctor and attaches the corresponding coordinates before sending to database
 def build_doctor_dict(doctors_csv):
     with open(doctors_csv) as fp:
         reader = csv.reader(fp)
@@ -56,6 +61,7 @@ def build_doctor_dict(doctors_csv):
                 taxonomy = row[TAXONOMY_FIELD]
                 taxonomy_description = taxonomy_dict[taxonomy]
 
+                # Fills initial doctor information
                 doctor_dict = {'NPI': row[NPI_FIELD],
                                'Last Name': row[LAST_NAME_FIELD],
                                'First Name': row[FIRST_NAME_FIELD],
@@ -70,13 +76,21 @@ def build_doctor_dict(doctors_csv):
                                            'Zip Code': row[ZIP_CODE_FIELD]}
                                }
 
-                coord_state = doctor_dict.get('Address', None).get('State', None)
-                coord_city = doctor_dict.get('Address', None).get('City', None)
-                lat = coord_dict[coord_state][coord_city]
-                # lng = coord_dict[coord_state][coord_city]
-                # coll.insert(doctor_dict)
-            except:
+                # Gets the coordinates for the doctor's city
+                doc_state = doctor_dict.get('Address', None).get('State', None)
+                doc_city = doctor_dict.get('Address', None).get('City', None).title()
+                latitude = coord_dict[doc_state][doc_city][0][0]
+                longitude = coord_dict[doc_state][doc_city][0][1]
+
+                # Updates Address dictionary with coordinates
+                doctor_dict['Address']['Latitude'] = latitude
+                doctor_dict['Address']['Longitude'] = longitude
+
+                # Insert into database
+                coll.insert(doctor_dict)
+            except:# (IOError, RuntimeError) as e:
                 # TODO: exception handling needed
+                # print(e.message)
                 pass
 
 
@@ -84,14 +98,13 @@ def main(argv):
     if not len(argv) == 3:
         print("Usage: NPPES_parse.py <doctors_csv.csv> <taxonomy_csv.csv> <coordinates_csv.csv>")
     else:
-        try:
+        """try:
             # TODO: is this part needed?
-            # client = MongoClient()
+            client = MongoClient()
             print ("Connected to MongoDB successfully.")
         except ConnectionFailure, e:
             sys.stderr.write("Could not connect to MongoDB: %s" % e)
-            sys.exit(1)
-
+            sys.exit(1)"""
         build_coord_dict(argv[2])
         build_taxonomy_dict(argv[1])
         build_doctor_dict(argv[0])
